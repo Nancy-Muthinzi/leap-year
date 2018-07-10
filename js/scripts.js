9a7a1b15edafa58a3a40d8a517f82892d6e0da56
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-07-10 16:36:26 +0300

    add spec on js file

--- js/scripts.js
+++ js/scripts.js
@@ -9,15 +9,9 @@ $(document).ready(function() {
   });
 
   var leapYear = function(year) {
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+      return true;
+    } else {
       return false;
-    };
-
-    var leapYear = function(year) {
-      if (year % 100 === 0) {
-        return false;
-      } else if (year % 4 === 0) {
-        return true;
-      } else {
-        return false;
-      }
-    };
+    }
+  };
